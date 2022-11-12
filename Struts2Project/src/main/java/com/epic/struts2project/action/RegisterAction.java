3a7fd420a80e74e65af1892f621bc807c3c75db9
amayuru56789 <indeewara5678@gmail.com>
2022-11-12 20:35:26 +0530

    added DbConnection for StrutsProject

---
+++ Struts2Project/src/main/java/com/epic/struts2project/action/RegisterAction.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.epic.struts2project.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author Amayuru indeewara
+ */
+public class RegisterAction extends ActionSupport {
+    
+    @Override
+    public String execute(){
+        //return "REGISTER";
+        return SUCCESS;
+    }
+}
